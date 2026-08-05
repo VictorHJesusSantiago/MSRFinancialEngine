1f15a6625787550b0a162fd64736e41a14279bab
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-08-05 20:18:20 -0300

    refactor: Remove unnecessary XML comments from InitialCreate migration files

--- src/MSRFinancialEngine.Infrastructure/Persistence/Migrations/20260709224831_InitialCreate.cs
+++ src/MSRFinancialEngine.Infrastructure/Persistence/Migrations/20260709224831_InitialCreate.cs
@@ -1,14 +1,12 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace MSRFinancialEngine.Infrastructure.Persistence.Migrations
 {
-    /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -367,7 +365,6 @@ namespace MSRFinancialEngine.Infrastructure.Persistence.Migrations
                 unique: true);
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
